07f567245beb59277ca00ba68e8f36368267e1a7
Jathavaan Shankarr <jathavaan12@gmail.com>
2026-01-16 15:36:58 +0100

    Added db as db host

--- server/app/core/database.py
+++ server/app/core/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "db")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 
 DATABASE_URL = (
